8baeae7b935a1aa535b81347d1fd063e22bbdcfd
giuseppe600 <85023068+giuseppe600@users.noreply.github.com>
2021-05-30 15:00:00 +0200

    Update Configuration.h

--- config/examples/Creality/Ender-3 V2/Configuration.h
+++ config/examples/Creality/Ender-3 V2/Configuration.h
@@ -1237,8 +1237,8 @@
 // @section machine
 
 // The size of the printable area
-#define X_BED_SIZE 220
-#define Y_BED_SIZE 220
+#define X_BED_SIZE 400
+#define Y_BED_SIZE 400
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
